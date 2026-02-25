e3dfb5e8c18be491bec0ac717407f6405f03a826
Mousity <samr.mouna@gmail.com>
2026-02-25 18:36:58 -0500

    Added trash asset

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class TrashCan : MonoBehaviour, IInteractable
 {
     public StationType stationType;
+    private SpriteRenderer sr;
 
+
+    public void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = stationType.sprite;
+    }
     public void Interact(PlayerInventory inventory)
     {
         if(inventory.heldIngredientId != null)
